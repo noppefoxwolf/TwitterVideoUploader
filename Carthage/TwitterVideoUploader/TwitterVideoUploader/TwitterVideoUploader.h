159752447865b36c3cae5c4d698063908ad9b9f6
🦊Tomoya Hirano <noppelabs@gmail.com>
2017-10-21 03:52:17 +0900

    add shared scheme

---
+++ Carthage/TwitterVideoUploader/TwitterVideoUploader/TwitterVideoUploader.h
@@ -0,0 +1,19 @@
+//
+//  TwitterVideoUploader.h
+//  TwitterVideoUploader
+//
+//  Created by Tomoya Hirano on 2017/10/21.
+//  Copyright © 2017年 Tomoya Hirano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TwitterVideoUploader.
+FOUNDATION_EXPORT double TwitterVideoUploaderVersionNumber;
+
+//! Project version string for TwitterVideoUploader.
+FOUNDATION_EXPORT const unsigned char TwitterVideoUploaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TwitterVideoUploader/PublicHeader.h>
+
+
